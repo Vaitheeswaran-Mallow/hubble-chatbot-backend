deb9192c52a805d7d3dfa3457fe7db759c450125
Naveen <naveen.balasubramaniam@mallow-tech.com>
2025-09-27 12:31:19 +0530

    Create an POST endpoint to create timesheet entries

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     open_api_key: str
     ws_upstream_url: str | None = None
+    database_url: str = "sqlite:///./hubble.db"
 
     class Config:
         env_file = ".env"
