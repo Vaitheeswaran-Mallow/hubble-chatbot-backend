d41f233be90e7761ed8e82c0398e3c78c7213a7c
Palaniyappan Ramanathan <palaniyappan@mallow-tech.com>
2025-09-27 12:58:11 +0530

    Temp chat with docs

--- db.py
+++ db.py
@@ -1,5 +1,54 @@
+import os
+from pathlib import Path
+
 import chromadb
+from markitdown import MarkItDown
+from langchain.text_splitter import (
+    RecursiveCharacterTextSplitter,
+    CharacterTextSplitter,
+)
+
 
 def get_chroma_client():
     """Initialize and return a ChromaDB client."""
     return chromadb.PersistentClient(path="./chroma_db")
+
+
+def get_collection():
+    """Get or create a collection in ChromaDB."""
+    client = get_chroma_client()
+    return client.get_or_create_collection(name="documents")
+
+
+def convert_to_markdown(file_path: str) -> str:
+    """Convert a given file to markdown format using MarkItDown."""
+    md = MarkItDown(enable_plugins=False)
+    result = md.convert(file_path)
+    return result.text_content
+
+
+def character_splitter(text: str):
+    """Split text into smaller chunks using RecursiveCharacterTextSplitter."""
+    text_splitter = CharacterTextSplitter(
+        chunk_size=250,
+        chunk_overlap=100,
+        # length_function=len,
+        separator="\n\n",
+    )
+    return text_splitter.split_text(text)
+
+
+def add_document_to_db():
+    collection = get_chroma_client().get_or_create_collection(name="documents")
+    docs = os.listdir("./docs")
+    for doc in docs:
+        text = convert_to_markdown(f"./docs/{doc}")
+        chunks = character_splitter(text)
+        for chunk in chunks:
+            collection.add(
+                documents=[chunk], metadatas={"source": doc}, ids=[f"{doc}_{chunk}"]
+            )
+
+
+if __name__ == "__main__":
+    add_document_to_db()
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Depends
 from settings import config
-from langchain_openai import OpenAI
-from db import get_chroma_client
+# from langchain_openai import OpenAI
+from openai import OpenAI
+from db import get_chroma_client, get_collection
 from markitdown import MarkItDown
+from fastapi import Query
 
 app = FastAPI(title="Hubble Chatbot Backend")
+
 
 def get_markdown_converter():
     """Initialize and return a Markitdown instance."""
@@ -21,11 +24,11 @@ def get_config():
     return {"open_api_key": config.open_api_key}
 
 
-@app.get("/llm-test")
-def llm_test():
-    llm = OpenAI(api_key=config.open_api_key, temperature=0)
-    response = llm.invoke("What is the capital of India?")
-    return {"response": response}
+# @app.get("/llm-test")
+# def llm_test():
+#     llm = OpenAI(api_key=config.open_api_key, temperature=0)
+#     response = llm.invoke("What is the capital of India?")
+#     return {"response": response}
 
 
 @app.get("/db-test")
@@ -35,7 +38,36 @@ def db_test(chroma_db=Depends(get_chroma_client)):
     else:
         return {"status": "ChromaDB is not reachable"}
 
+
 @app.get("/convert-to-markdown/")
 def convert_to_markdown(md=Depends(get_markdown_converter)):
     result = md.convert("./docs/New Leave policy.pdf")
     return {"converted_text": result}
+
+
+@app.get("/list-collections/")
+def list_collections(chroma_db=Depends(get_chroma_client)):
+    collections = chroma_db.list_collections()
+    return {"collections": collections}
+
+
+@app.get("/query-leave-policy/")
+def query_leave_policy(query: str = Query(...), chroma_db=Depends(get_collection)):
+    results = chroma_db.query(query_texts=[query], n_results=5)
+    print(results)
+    client = OpenAI(api_key=config.open_api_key)
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant that helps employees with their HR related queries based on the company policies.",
+        },
+        {
+            "role": "user",
+            "content": f"Answer the question based on the context below. If you don't know the answer, just say that you don't know, don't try to make up an answer. Context: {results['documents']}",
+        },
+        {"role": "user", "content": query},
+    ]
+    response = client.responses.create(
+        model="gpt-4o-mini", input=messages
+    )
+    return {"response": response.output_text}
