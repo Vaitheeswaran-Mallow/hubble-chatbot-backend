046e016171c86ee04532ff0f82bfae43e2aa2858
Palaniyappan Ramanathan <palaniyappan@mallow-tech.com>
2025-09-27 11:32:40 +0530

    Add llm-test API

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from settings import config
+from langchain_openai import OpenAI
 
 app = FastAPI(title="Hubble Chatbot Backend")
 
@@ -12,3 +13,10 @@ def hello_world():
 @app.get("/config")
 def get_config():
     return {"open_api_key": config.open_api_key}
+
+
+@app.get("/llm-test")
+def llm_test():
+    llm = OpenAI(api_key=config.open_api_key, temperature=0)
+    response = llm.invoke("What is the capital of India?")
+    return {"response": response}
